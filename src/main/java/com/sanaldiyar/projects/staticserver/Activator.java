c4f2862f3909b3bb1041faeb329dc80f67ac3507
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2013-11-02 09:40:03 +0200

    init

---
+++ src/main/java/com/sanaldiyar/projects/staticserver/Activator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sanaldiyar.projects.staticserver;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ *
+ * @author kazim
+ */
+public class Activator implements BundleActivator{
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        
+    }
+    
+}
